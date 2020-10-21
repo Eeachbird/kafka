186bc5178c3dc195c3f420b637aa9991bbc241b4
liujinyong <893296814@qq.com>
2020-10-21 17:31:24 +0800

    dev1

---
+++ src/Kafka.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created By PHPSTROM
+ * User : JinYong Liu
+ * Date : 2020/10/21 5:27 下午
+ * ProjectInfo :Kafka.php
+ */
+namespace kafka\test;
+Class  kafkaExtension
+{
+
+    public function Send()
+    {
+        echo 123;
+    }
+}
